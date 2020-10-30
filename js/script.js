7f8712a6457e4e6e97c9d3453f93607c045e987a
ericpangiawan <ericpangiawan@gmail.com>
2020-10-31 01:55:21 +0800

    Initial Commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 $(document).ready(() => {
     $(window).scroll(function(){
-        if(window.innerWidth >= 992){
+        if($(window).width() >= 992){
             if($(window).scrollTop() >= 50){
                 //jika discroll ke bawah
-                $('.navbar').css('background-color', 'white');
+                $('.navbar').css({
+                    'background-color': 'white',
+                    'transition': 'ease 500ms'
+                });
                 $('.nav-color').css('color', 'black');
-                $('.navbar').fadeIn();
+                $('.nav-link').toggleClass('changed', true);
             }else{
                 $('.navbar').css('background-color', 'transparent');
                 $('.nav-color').css('color', 'white');
+                $('.nav-link').toggleClass('changed', false);
             }
+        }
+    });
+    if($(window).scrollTop() >= 50){
+        $('.navbar').css('background-color', 'white');
+        $('.nav-color').css('color', 'black');
+        $('.nav-link').toggleClass('changed', true);
+    }
+    $(window).resize(function(){
+        if($(window).width() < 992){
+            $('.navbar').css('background-color', 'white');
+            $('.nav-color').css('color', 'black');
         }
     });
 })
